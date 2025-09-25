db35a7ca3a7eb7f20f4ddc314a68cf277dc92633
Ілля Захарольский <vonersengame@gmail.com>
2025-09-25 15:20:30 +0200

    Add files via upload

---
+++ Homework_Lists_Dictionaries/EntryPoint/Program.cs
@@ -0,0 +1,27 @@
+﻿using Homework_Lists_Dictionaries.EntryPoint.Handlers;
+using Homework_Lists_Dictionaries.EntryPoint.Menus;
+using System;
+
+namespace Homework_Lists_Dictionaries.EntryPoint;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        while (true)
+        {
+            try
+            {
+                Console.Clear();
+                MainMenu.Display();
+                MainHandler.Start();
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Homework_Lists_Dictionaries/ProgramHeart/Core/Interfaces/IRunnable.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Homework_Lists_Dictionaries.ProgramHeart.Core.Interfaces;
+
+public interface IRunnable
+{
+    void Run();
+}
