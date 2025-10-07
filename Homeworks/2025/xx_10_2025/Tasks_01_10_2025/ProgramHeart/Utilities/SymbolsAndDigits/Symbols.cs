8dafa5430911a323a4bb268e4b3da8fe79daf41d
Ілля Захарольский <vonersengame@gmail.com>
2025-10-07 17:21:24 +0200

    Delete Homeworks/2025 directory

--- Homeworks/2025/xx_10_2025/Tasks_01_10_2025/ProgramHeart/Utilities/SymbolsAndDigits/Symbols.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Tasks_01_10_2025.ProgramHeart.Utilities.SymbolsAndDigits;
-
-internal static class Symbols
-{
-    internal static readonly string symbols = "°^!§$%&/()=?`´*-+~#<>|@€;:_,.";
-}
