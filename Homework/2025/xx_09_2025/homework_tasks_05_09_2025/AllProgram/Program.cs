7f71499b204fe1d7923ad1296183454df2fc7a96
Ілля Захарольский <vonersengame@gmail.com>
2025-10-21 22:16:19 +0200

    Add files via upload

---
+++ Homework/2025/xx_09_2025/homework_tasks_05_09_2025/AllProgram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tasks_05_09_2025
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Clear();
+                    Menu.StartMenu();
+                    ChoiceHandler.StartHandler();
+                    Console.ReadKey();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error! {ex.Message}");
+                }
+            }
+        }
+    }
+}
+
